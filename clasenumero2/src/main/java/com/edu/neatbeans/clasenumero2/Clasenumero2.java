134f498006770d70cfe632037ae3cde39c446e67
luckschavalo <lucaschl1007@gmail.com>
2024-06-05 18:04:38 -0600

    Add files via upload

---
+++ clasenumero2/src/main/java/com/edu/neatbeans/clasenumero2/Clasenumero2.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edu.neatbeans.clasenumero2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lucas
+ */
+public class Clasenumero2 {
+
+    public static void main(String[] args) {
+        if (a == 1 && b == 3){
+            JOptionPane.
+        }
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+        /*
+        String nombreParaSalario;
+        nombreParaSalario = JOptionPane.showInputDialog(null,"digite su nombre:");
+        double salarioMensual = double.parseDouble(JOptionPane.showInputDialog(null, mesasage: "Digite su salario semanal:"));
+        double salarioBruto = salariomensual*4;
+        double salarioNeto = salario bruto * 0.0934;
+        JOptionPane.showConfirmDialog(parentComponent, args)
+      /*
+    
+
+
+       
+                
+       
+    
+    
+    } 
+  
+}
